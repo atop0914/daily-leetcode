a7c3d8d5d973af70207c7fd6b5743d8a50315d7a
zhuyitao <zhuyitao@lanjingren.com>
2021-11-15 21:14:25 +0800

    best-time-to-buy-and-sell-stock-i

---
+++ 121.买卖股票的最佳时机.go
@@ -0,0 +1,35 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=121 lang=golang
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	maxProfit, minCost, days := 0, prices[0], len(prices)
+
+	for i := 1; i < days; i++ {
+		minCost = min_121(minCost, prices[i])
+		maxProfit = max_121(prices[i]-minCost, maxProfit)
+	}
+
+	return maxProfit
+}
+
+func max_121(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min_121(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+// @lc code=end
