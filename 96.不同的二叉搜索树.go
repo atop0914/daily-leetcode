5f6da0253086e80cf370a2666938746f10fcf4d0
zhuyitao <zhuyitao@lanjingren.com>
2021-11-22 21:17:52 +0800

    unique-binary-search-trees

---
+++ 96.不同的二叉搜索树.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=96 lang=golang
+ *
+ * [96] 不同的二叉搜索树
+ */
+
+// @lc code=start
+func numTrees(n int) int {
+	// 结题思路：假设n个节点存在二叉排序树的个数是G(n)，1为根节点，2为根节点，...，n为根节点，当1为根节点时，其左子树节点个数为0，右子树节点个数为n-1，同理当2为根节点时，其左子树节点个数为1，右子树节点为n-2，所以可得G(n) = G(0)*G(n-1)+G(1)*(n-2)+...+G(n-1)*G(0)
+	// 二叉搜索树 左子树小于根节点 右子树大于根节点
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	// dp[2] = 2
+	// dp[3] = 5
+
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+
+	return dp[n]
+}
+
+// @lc code=end
