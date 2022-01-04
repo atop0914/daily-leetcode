febc8bcb84ba5eb4dca0e136201e51b14d669417
zhuyitao <zhuyitao@lanjingren.com>
2022-01-04 20:45:00 +0800

    intersection-of-two-linked-lists

---
+++ 160.相交链表.go
@@ -0,0 +1,43 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=160 lang=golang
+ *
+ * [160] 相交链表
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+
+	p1, p2 := headA, headB
+
+	for p1 != p2 {
+		if p1 == nil {
+			p1 = headB
+		} else {
+			p1 = p1.Next
+		}
+
+		if p2 == nil {
+			p2 = headA
+		} else {
+			p2 = p2.Next
+		}
+	}
+
+	return p1
+}
+
+// @lc code=end
